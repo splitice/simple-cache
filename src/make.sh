cbb70512d9ba7c1a1b89a31a877d5421652629a4
splitice <mat999@gmail.com>
2014-07-07 01:58:52 +1000

    added debug option to build script

--- src/make.sh
+++ src/make.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-COMPILE_OPTIONS="-lpcap"
+COMPILE_OPTIONS=""
+if [[ "$DEBUG" ]]; then
+	COMPILE_OPTIONS="-g"
+fi
 
 gcc scache.c db.c hash.c connection.c http.c $COMPILE_OPTIONS -std=c99 -D_POSIX_C_SOURCE=200809L -O3 -o scache
