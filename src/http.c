50d73d7b51858d0459048676a506e608f0898072
splitice <mat999@gmail.com>
2014-07-07 16:05:53 +1000

    attempt using static for now

--- src/http.c
+++ src/http.c
@@ -26,14 +26,6 @@
 
 char misc_buffer[4096];
 
-const char http_templates[NUMBER_OF_HTTPTEMPLATE][100] = {
-	"HTTP/1.1 200 OK\r\nConnection: Keep-Alive\r\n",
-	"HTTP/1.1 404 File Not Found\r\nnConnection: Keep-Alive\r\nContent-Length: 4\r\n404-\r\n\r\n",
-	"HTTP/1.1 200 OK\r\nnConnection: Keep-Alive\r\nContent-Length: 2\r\nOK\r\n\r\n",
-	"\r\n\r\n",
-	"HTTP/1.1 400 Bad Request\r\nnConnection: Close\r\nContent-Length: 14\r\nInvalid Method\r\n\r\n",
-};
-
 /*
 Handle read data for the connection
 
