58f63a48783afe997c123233a14103c41b861e01
splitice <mat999@gmail.com>
2014-07-27 01:12:19 +1000

    fix typo compilation error

--- src/core/db.cpp
+++ src/core/db.cpp
@@ -581,7 +581,7 @@ void db_target_write_allocate(struct cache_target* target, uint32_t data_length)
 		if (IS_SINGLE_FILE(entry)){
 			//Shorten or lengthen file to appropriate size
 			if (ftruncate(target->fd, data_length)<0){
-				PWARN("File truncation failed (fd: %d, length: %d)", target->fd, data_length;
+				PWARN("File truncation failed (fd: %d, length: %d)", target->fd, data_length);
 			}
 		}
 		else{
