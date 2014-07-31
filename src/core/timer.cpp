386a7ff9b5a3dec428ca0d770e7a4e9f5c28472f
splitice <mat999@gmail.com>
2014-08-01 03:54:45 +1000

    increase the delay to accomodate heavily loaded travis-ci instances

--- src/core/timer.cpp
+++ src/core/timer.cpp
@@ -34,10 +34,10 @@ void timer_setup()
 
 	/* Configure the timer to expire after 1 sec... */
 	timer.it_value.tv_sec = 0;
-	timer.it_value.tv_usec = 250000;
+	timer.it_value.tv_usec = 500000;
 	/* ... and every 1 sec after that. */
 	timer.it_interval.tv_sec = 0;
-	timer.it_interval.tv_usec = 250000;
+	timer.it_interval.tv_usec = 500000;
 	/* Start a virtual timer. It counts down whenever this process is
 	executing. */
 	if (setitimer(ITIMER_REAL, &timer, NULL) < 0){
