3062ccc4f6964658beeda844bec060ac5714a7be
splitice <mat999@gmail.com>
2014-08-11 10:42:11 +1000

    dry up expires header

--- src/core/http_respond.cpp
+++ src/core/http_respond.cpp
@@ -25,20 +25,23 @@
 #include "http_parse.h"
 #include "http_respond.h"
 
+void copy_to_outputbuffer(cache_connection* connection, char* header, int length){
+	connection->output_buffer = connection->output_buffer_free = (char*)malloc(length);
+	memcpy(connection->output_buffer_free, header, length);
+	connection->output_length = length;
+}
+
 state_action http_respond_expires(int epfd, cache_connection* connection){
 	int fd = connection->client_sock;
-	char ttl[128];
+	char header[32];
 
 	if (REQUEST_IS(connection->type, REQUEST_GETKEY) || REQUEST_IS(connection->type, REQUEST_HEADKEY)){
 		DEBUG("[#%d] Responding with X-Ttl\n", fd);
 		//Returns the number of chars put into the buffer
 		__time_t expires = connection->target.key.entry->expires;
-		int temp = snprintf(ttl, 128, "X-Ttl: %d\r\n", (expires == 0) ? expires : (expires - time_seconds));
+		int temp = snprintf(header, sizeof(header), "X-Ttl: %d\r\n", (expires == 0) ? expires : (expires - time_seconds));
 
-		connection->output_buffer_free = (char*)malloc(temp);
-		memcpy(connection->output_buffer_free, ttl, temp);
-		connection->output_buffer = connection->output_buffer_free;
-		connection->output_length = temp;
+		copy_to_outputbuffer(connection, header, temp);
 	}
 	connection->handler = http_respond_contentlength;
 	return continue_processing;
@@ -186,12 +189,6 @@ state_action http_respond_listing(int epfd, cache_connection* connection){
 	}
 
 	return close_connection;
-}
-
-void copy_to_outputbuffer(cache_connection* connection, char* header, int length){
-	connection->output_buffer = connection->output_buffer_free = (char*)malloc(length);
-	memcpy(connection->output_buffer_free, header, length);
-	connection->output_length = length;
 }
 
 state_action http_respond_listingtotal(int epfd, cache_connection* connection){
